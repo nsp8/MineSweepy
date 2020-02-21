2d59e3858bc507982599ed9469afbae866b57a52
Nishant Parmar <55044190+nsp8@users.noreply.github.com>
2020-02-21 12:16:04 +0530

    Implementing a naive Minesweeper

---
+++ main.py
@@ -0,0 +1,121 @@
+from os import system
+import util
+
+
+class BattleGround:
+    class MineNode:
+
+        def __init__(self):
+            self.value = 0
+            self.facade = "*"
+            self.x = 0
+            self.y = 0
+
+        def create_node(self, x, y, value=0):
+            self.x = x
+            self.y = y
+            self.value = value
+
+        def __repr__(self):
+            return "{}".format(self.facade)
+
+    def __init__(self, n=8):
+        self.grid = list()
+        self.side = n
+        self.moves = list()
+        self.status = "hot"
+
+    def initialize_grid(self):
+        for i in range(self.side):
+            row = list()
+            for j in range(self.side):
+                # print("\ni={}\tj={}".format(i, j))
+                new_node = self.MineNode()
+                new_node.create_node(i, j)
+                row.append(new_node)
+            self.grid.append(row)
+
+    def deploy_mines(self):
+        mines = util.define_mines(self.side)
+        neighbors = list()
+        for mine in mines:
+            x, y = mine
+            self.grid[x][y].value = "m"
+            neighbors.extend(util.get_neighbours(mine, self.side))
+        for node in neighbors:
+            r, c = node
+            node_value = self.grid[r][c].value
+            if isinstance(node_value, int):
+                self.grid[r][c].value += 1
+
+    def display(self):
+        output = util.print_grid_skeleton(self)
+        print(output)
+
+
+def re_prompt(preface=""):
+    main_msg = "Press Return to play again, or any other key to exit"
+    prompt_msg = "\n{}\n{}\n".format(preface, main_msg)
+    action = input(prompt_msg)
+    return action.strip()
+
+
+if __name__ == "__main__":
+    while True:
+        try:
+            system(util.clear_console())
+            ground_size = input("\nEnter the size of the field (a whole number"
+                                " greater than 4 but not more than 10)?: ")
+            if ground_size.isnumeric():
+                ground_size = int(ground_size.strip())
+                if ground_size not in range(4, 11):
+                    print("\nField size either too small or too large! "
+                          "Please try again.\n")
+            else:
+                print("\nInvalid input - defaulting to a field size of 8")
+                ground_size = 8
+            new_ground = BattleGround(ground_size)
+            new_ground.initialize_grid()
+            new_ground.deploy_mines()
+            win_count = ground_size * (ground_size - 1)
+            print("\nThe mines are deployed in the field! "
+                  "Proceed with caution!\n")
+            new_ground.display()
+            while new_ground.status == "hot":
+                if len(new_ground.moves) == win_count:
+                    new_ground.status = "cold"
+                    break
+                user_selection = input("Type in the cell you want to reveal \n("
+                                       "example - A8 will reveal the first cell"
+                                       " in the 8th row): ")
+                user_selection = user_selection.strip()
+                if user_selection:
+                    if user_selection in new_ground.moves:
+                        print("\nThis cell was already played. "
+                              "Please try again!\n")
+                        continue
+                    selection_valid = util.validate_user_selection(
+                        user_selection, ground_size)
+                    if selection_valid:
+                        new_ground.moves.append(user_selection)
+                        new_ground.display()
+                    else:
+                        print("Invalid selection! Try again.\n")
+                        new_ground.display()
+                        continue
+                else:
+                    print("\nSorry, no input received! Give it another go.\n")
+                    new_ground.display()
+                    continue
+            if new_ground.status == "detonated":
+                banner = "#" * 50
+                if re_prompt(preface=banner):
+                    break
+            if new_ground.status == "cold":
+                banner = "*" * 50
+                print("{}\nYou won!\n{}".format(banner, banner))
+                if re_prompt():
+                    break
+        except Exception as e:
+            if re_prompt(preface="Oops! An exception occurred: {}".format(e)):
+                break
---
+++ util.py
@@ -0,0 +1,93 @@
+from platform import system
+from random import randint
+from math import floor
+import re
+
+
+def clear_console():
+    os_ = system().lower()
+    if os_ == "windows":
+        return "cls"
+    elif os_ in ["linux", "mac", "os x"]:
+        return "clear"
+    else:
+        return ""
+
+
+def map_headers(size):
+    col_head, row_head = dict(), dict()
+    for val in range(size):
+        col_head[val] = chr(65 + val)
+        row_head[val] = 1 + val
+    return col_head, row_head
+
+
+def reverse_mapping(object_map):
+    return {v: k for k, v in object_map.items()}
+
+
+def print_grid_skeleton(grid_object):
+    display = ""
+    grid, moves = grid_object.grid, grid_object.moves
+    n = len(grid)
+    col_id, row_id = map_headers(n)
+    display += "\t{}\n".format(" ".join(col_id.values()))
+    if len(moves) == 0:
+        default_node_value = "* " * n
+        for i in range(n):
+            display += "{}\t{}\n".format(row_id[i], default_node_value)
+    else:
+        most_recent_move = moves[-1]
+        row_mapped = reverse_mapping(row_id)[int(most_recent_move[1])]
+        col_mapped = reverse_mapping(col_id)[most_recent_move[0]]
+        grid_pos = grid[row_mapped][col_mapped]
+        if grid_pos.value != "m":
+            grid[row_mapped][col_mapped].facade = grid_pos.value
+            for row in grid:
+                val_ = " ".join(str(node.facade) for node in row)
+                display += "{}\t{}\n".format(row_id[grid.index(row)], val_)
+        else:
+            for row in grid:
+                val_ = " ".join(str(node.value) for node in row)
+                display += "{}\t{}\n".format(row_id[grid.index(row)], val_)
+            print("You hit a mine!\nX-P")
+            grid_object.status = "detonated"
+    return display
+
+
+def define_mines(n=8):
+    mines = list()
+    while len(mines) != n:
+        pos_x = randint(0, n - 1)
+        pos_y = randint(0, n - 1)
+        mine_pos = (pos_x, pos_y)
+        if mine_pos not in mines:
+            mines.append(mine_pos)
+    return mines
+
+
+def get_neighbours(node, n=8):
+    x, y = node
+    values = [(x - 1, y - 1), (x, y - 1), (x + 1, y - 1), (x - 1, y),
+              (x + 1, y), (x - 1, y + 1), (x, y + 1), (x + 1, y + 1)]
+    value_combos = []
+    for value in values:
+        xn, yn = value
+        if xn in range(n) and yn in range(n):
+            value_combos.append(value)
+    return value_combos
+
+
+def validate_user_selection(user_value, size):
+    allowed_selection_length = 2 + floor(size / 10)
+    if len(user_value) <= allowed_selection_length:
+        match = re.search(r"(\w)(\d+)", user_value)
+        if match:
+            matches = match.groups()
+            col_part, row_part = matches
+            row_part = int(row_part)
+            col_headers, _ = map_headers(size)
+            col_headers_inv = reverse_mapping(col_headers)
+            if row_part <= size and col_part in col_headers_inv.keys():
+                return True
+    return False
